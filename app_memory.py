e1a0df801f40e340485e502b44f0c7224e0ce2dd
lucseghers <168024469+lucseghers@users.noreply.github.com>
2025-11-23 11:39:14 +0100

    Add files via upload

--- app_memory.py
+++ app_memory.py
@@ -52,7 +52,7 @@ def create_text_card(text, filename, img_dir,
     except Exception:
         font = ImageFont.load_default()
 
-    wrapped = fill(text, width=15)
+    wrapped = fill(text, width=8) #15 vervangen door 8
     bbox = draw.multiline_textbbox((0, 0), wrapped, font=font, align="center")
     tw = bbox[2] - bbox[0]
     th = bbox[3] - bbox[1]
