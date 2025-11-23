79d80e3cfe3fbac50a9d5b37e695911e5e080d3d
lucseghers <168024469+lucseghers@users.noreply.github.com>
2025-11-23 11:35:11 +0100

    Add files via upload
    groter font

--- app_memory.py
+++ app_memory.py
@@ -36,7 +36,7 @@ def parse_pairs(text: str):
 
 
 def create_text_card(text, filename, img_dir,
-                     img_width=300, img_height=150,
+                     img_width=600, img_height=400,
                      bg_color=(255, 255, 255),
                      text_color=(0, 0, 0)):
     """
